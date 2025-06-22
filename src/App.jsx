08e85889a4f92bc187c47373912a92f852833a89
Aman Gulia <amangulia4610@gmail.com>
2025-06-22 12:19:35 -0400

    Refactor code structure for improved readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Room from './components/Room.jsx'
-import Home from './components/Home.jsx'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Room from "./components/Room.jsx";
+import Home from "./components/Home.jsx";
 const App = () => {
   return (
-    
     <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/room/:roomID' element={<Room/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/room/:roomID" element={<Room />} />
     </Routes>
-  
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,44 +1,54 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
 const Room = () => {
-    const { roomID } = useParams();
-    const myMeeting=async(element) => {
-    const appId=1501307780;
-    const serverSecret='cf77448938fe0a8a6f97f18ffafc6ce5';
-          const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appId, serverSecret, roomID,  Math.random().toString(36).substring(2, 10),"User 1");
-     const zp = ZegoUIKitPrebuilt.create(kitToken);
-    
-       // start the call
-      zp.joinRoom({
-        container: element,
-        sharedLinks: [
-          {
-            name: 'Copy link',
-            url:
-             window.location.protocol + '//' + 
-             window.location.host + window.location.pathname + '#/room/' + roomID,
-          },
-        ],
-        scenario: {
-          mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+  const { roomID } = useParams();
+  const myMeeting = async (element) => {
+    const appId = 1501307780;
+    const serverSecret = "cf77448938fe0a8a6f97f18ffafc6ce5";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appId,
+      serverSecret,
+      roomID,
+      Math.random().toString(36).substring(2, 10),
+      "User 1"
+    );
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+    // start the call
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: "Copy link",
+          url:
+            window.location.protocol +
+            "//" +
+            window.location.host +
+            window.location.pathname +
+            "#/room/" +
+            roomID,
         },
-      });
-
-    }
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+      },
+    });
+  };
   return (
-    
-    <div id='room'>
+    <div id="room">
+      <div ref={myMeeting}></div>
 
-            <div ref={myMeeting}></div>
-
-        <button onClick={() => {
-            window.location.href = '/react-video-call-aman/'; // Adjust the path as needed
-        }}>Back to Home</button>
+      <button
+        onClick={() => {
+          window.location.href = "/react-video-call-aman/"; // Adjust the path as needed
+        }}
+      >
+        Back to Home
+      </button>
     </div>
+  );
+};
 
-  )
-}
-
-export default Room
+export default Room;
